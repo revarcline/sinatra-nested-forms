90c3122fd6e066171d97dc470ca864eef83b018b
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-21 23:50:46 -0600

    commit tree 6099fdad1fb777d2bb0fd5de6e161421119b0493

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
+# the age of sail indeed
 class Ship
+  attr_accessor :name, :type, :booty
+
+  class << self
+    attr_accessor :all
+  end
+
+  @all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+
+    self.class.all << self
+  end
+
+  def self.clear
+    @all = []
+  end
 end
